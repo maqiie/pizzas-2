6bd399c0f0830a64197cabb4ea695c0099cb96c4
maqiie <99504010+maqiie@users.noreply.github.com>
2023-03-27 16:17:48 +0300

    seeded

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :restaurants, only: [:index, :show, :destroy] do
-    resources :pizzas, only: [:index], controller: 'restaurant_pizzas'
-  end
-  resources :pizzas, only: [:index]
-  resources :restaurant_pizzas, only: [:create]
+  resources :pizzas
+  resources :restaurant_pizzas
+  resources :restaurants
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+#generate fake data for pizza model
+10.times do
+    Pizza.create(
+        name: Faker::Food.dish,
+        ingredients: Faker::Food.ingredients
+    )
+end
+
+# Generate fake data for Restaurant model
+5.times do
+    Restaurant.create(
+      name: Faker::Restaurant.name,
+      address: Faker::Address.full_address
+    )
+end
+
+# Generate fake data for RestaurantPizza model
+10.times do
+    RestaurantPizza.create(
+      price: Faker::Commerce.price(range: 1..30.0),
+      pizza_id: rand(1..10),
+      restaurant_id: rand(1..5)
+    )
+end
